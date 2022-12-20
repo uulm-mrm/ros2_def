a4ce4027b17f5ab0fd883def5e23f0e7a659737e
Jonas Otto <edwin.otto@uni-ulm.de>
2022-12-20 18:03:15 +0100

    parallel path detection

--- orchestrator_dummy_nodes/graph_analysis.py
+++ orchestrator_dummy_nodes/graph_analysis.py
@@ -9,6 +9,13 @@ from dataclasses import dataclass
 
 skip_topics = ["/rosout", "/parameter_events"]
 
+def all_equal(iterator):
+    iterator = iter(iterator)
+    try:
+        first = next(iterator)
+    except StopIteration:
+        return True
+    return all(first == x for x in iterator)
 
 @dataclass(frozen=True)
 class Topic:
@@ -58,17 +65,32 @@ def create_test_graph() -> nx.DiGraph:
 
 
 def analyze(DG: nx.DiGraph):
-    event_list = []
+    if len(list(nx.simple_cycles(DG))) > 1:
+        raise RuntimeError("Graph has cycle!", nx.find_cycle(DG))
+
     sensor_nodes = []
     for n in DG.nodes:
         if isinstance(n, Node) and DG.in_degree(n) == 0:
             sensor_nodes.append(n)
     print("Sensor nodes:", sensor_nodes)
 
+    merge_nodes = []
+
     for sn in sensor_nodes:
         print("Analyzing input from sensor node", sn)
-        for n in DG.successors(sn):
-            print(n)
+        descendants = list(nx.descendants(DG, sn))
+        descendants.sort(key = lambda n: nx.shortest_path_length(DG, sn, n))
+        for target in descendants:
+            paths = list(nx.all_simple_paths(DG, sn, target))
+            if len(paths) > 1:
+                inputs = []
+                for path in paths:
+                    inputs.append(path[-2])
+                if all_equal(inputs):
+                    # TODO: Multiple inputs on same topic from different sources
+                    continue
+                print("Warning: Multiple paths to", target, ":", inputs)
+                merge_nodes.append(target)
 
 
 class Analyzer(Node):
@@ -113,7 +135,7 @@ class Analyzer(Node):
 def main(args=None):
 
     DG = create_test_graph()
-    pos = nx.nx_agraph.graphviz_layout(DG, prog = "sfdp")
+    pos = nx.nx_agraph.graphviz_layout(DG, prog="sfdp")
     nx.draw(DG, pos=pos, with_labels=True)
     analyze(DG)
     plt.show()
