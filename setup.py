8350fcaf2e367ab909340c0fa402b998e0bdb4a3
Jonas Otto <edwin.otto@uni-ulm.de>
2023-01-10 16:29:18 +0100

    install launchfiles

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+from glob import glob
+import os
 from setuptools import setup
 
 package_name = 'orchestrator_dummy_nodes'
@@ -10,6 +12,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*'))
+
     ],
     install_requires=['setuptools'],
     zip_safe=True,
