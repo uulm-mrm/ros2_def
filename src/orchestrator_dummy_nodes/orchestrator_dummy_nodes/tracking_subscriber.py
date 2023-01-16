4488d4a1f8557080762da6f868d510636a8715db
Jonas Otto <edwin.otto@uni-ulm.de>
2023-01-16 14:35:06 +0100

    TrackingSubscriber inputs

--- src/orchestrator_dummy_nodes/orchestrator_dummy_nodes/tracking_subscriber.py
+++ src/orchestrator_dummy_nodes/orchestrator_dummy_nodes/tracking_subscriber.py
@@ -14,19 +14,43 @@ class TrackingSubscriber(Node):
         self.radar_subscription = self.create_subscription(String, "input_radar", self.radar_callback, 10)
         self.status_publisher = self.create_publisher(Status, "status", 10)
         self.tracks_publisher = self.create_publisher(String, "tracks", 10)
+        self.cached_camera = None
+        self.cached_radar = None
 
     def publish_status(self):
         status_msg = Status()
         status_msg.node_name = self.get_name()
         self.status_publisher.publish(status_msg)
 
-    def camera_callback(self, msg: String):
-        self.publish_status()
-
     def lidar_callback(self, msg: String):
+        if self.cached_camera is None:
+            self.get_logger().warn("Received lidar data without camera!")
+            self.publish_status()
+            return
+        camera = self.cached_camera
+        self.cached_camera = None
+
+        if self.cached_radar is None:
+            self.get_logger().warn("Received lidar data without radar!")
+            self.publish_status()
+            return
+        radar = self.cached_radar
+        self.cached_radar = None
+
+        tracks = String()
+        tracks.data = f"Tracks from inputs: [{camera.data}, {radar.data}, {msg.data}]"
+        self.tracks_publisher.publish(tracks)
+
+    def camera_callback(self, msg: String):
+        if self.cached_camera is not None:
+            self.get_logger().warn("Overwriting saved camera detection!")
+        self.cached_camera = msg
         self.publish_status()
 
     def radar_callback(self, msg: String):
+        if self.cached_radar is not None:
+            self.get_logger().warn("Overwriting saved radar detection!")
+        self.cached_radar = msg
         self.publish_status()
 
 
