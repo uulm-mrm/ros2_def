4a4fd2b1d564926faf7a700fa59e71c94331e293
Jonas Otto <edwin.otto@uni-ulm.de>
2023-03-08 10:59:18 +0100

    launch orchestrator in service launchfile

--- src/orchestrator_dummy_nodes/launch/service_test_orchestrated_launch.py
+++ src/orchestrator_dummy_nodes/launch/service_test_orchestrated_launch.py
@@ -1,15 +1,30 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, TextSubstitution, LaunchConfiguration
 
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 from orchestrator.orchestrator_lib.remapping_generation import generate_remappings_from_config
 
 
 def generate_launch_description():
     return LaunchDescription([
+        DeclareLaunchArgument(
+            "log_level",
+            default_value=TextSubstitution(text=str("info")),
+            description="Logging level"
+        ),
+        Node(
+            package='orchestrator_dummy_nodes',
+            executable='orchestrator',
+            name='orchestrator',
+            parameters=[
+                {"mode": "service"},
+            ],
+            arguments=['--ros-args', '--log-level', ['l:=', LaunchConfiguration('log_level')]]
+        ),
         *generate_remappings_from_config("orchestrator_dummy_nodes", "service_test_launch_config.json"),
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
@@ -17,6 +32,7 @@ def generate_launch_description():
                     FindPackageShare('orchestrator_dummy_nodes'),
                     'service_test_launch.py'
                 ])
-            ])
+            ]),
+            launch_arguments={"log_level": "warn"}.items()
         )
     ])
