d0e7c9de5e08876063ef661b48e6fb47634f87b2
Jonas Otto <edwin.otto@uni-ulm.de>
2023-07-06 09:49:40 +0000

    tracking_mock hashing

--- ros2/orchestrator_dummy_nodes/orchestrator_dummy_nodes/tracking_mock.py
+++ ros2/orchestrator_dummy_nodes/orchestrator_dummy_nodes/tracking_mock.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import hashlib
 
 import rclpy
 from rclpy.node import Node
@@ -14,11 +15,16 @@ class TrackingSubscriber(Node):
         self.status_publisher = self.create_publisher(Status, "status", 10)
         self.tracks_publisher = self.create_publisher(ObjectList, "tracks", 10)
         self.subs = []
-        for (i, j) in [(0, 2), (1, 1), (1, 2), (2, 1), (2, 2), (3, 1), (4, 1), (4, 2), (5, 1), (6, 1), (6, 2)]:
-            sub = self.create_subscription(DetectionList, f"amqp/detections{i}_{j}", self.omit_tracks_cb, 10)
-            self.subs.append(sub)
+        self.hash_state = hashlib.md5()
 
-        self.subs.append(self.create_subscription(DetectionList, "amqp/detections0_1", self.output_tracks_cb, 10))
+        self.declare_parameter("inputs")
+        input_topics = self.get_parameter("inputs").get_parameter_value().string_array_value
+
+        for it in input_topics:
+            self.subs.append(self.create_subscription(
+                DetectionList, it,
+                lambda msg, topic_name=it: self.omit_tracks_cb(msg, topic_name),
+                10))
 
     def output_tracks_cb(self, msg):
         self.get_logger().info("Received input, publishing tracks")
@@ -26,8 +32,11 @@ class TrackingSubscriber(Node):
         tracks_msg.header.frame_id = "mec_fusion"
         self.tracks_publisher.publish(tracks_msg)
 
-    def omit_tracks_cb(self, msg):
-        self.get_logger().info("Received input, not publishing tracks")
+    def omit_tracks_cb(self, msg, topic):
+        self.get_logger().info(f"Received input on {topic}, not publishing tracks. Message: {msg}")
+        self.hash_state.update(str.encode(topic))
+        self.hash_state.update(str.encode(f"{msg}"))
+
         status_msg = Status()
         status_msg.node_name = self.get_name()
         status_msg.omitted_outputs = [self.tracks_publisher.topic_name]
@@ -42,6 +51,9 @@ def main(args=None):
         rclpy.spin(tracking_subscriber)
     except KeyboardInterrupt:
         pass
+
+    tracking_subscriber.get_logger().fatal(
+        f"Final state of node {tracking_subscriber.get_name()}: {tracking_subscriber.hash_state.hexdigest()}")
 
     tracking_subscriber.destroy_node()
     rclpy.try_shutdown()
