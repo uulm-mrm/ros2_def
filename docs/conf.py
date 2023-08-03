045850356592b39bffe95ebe1a961aba94d45037
Jonas Otto <jonas@jonasotto.com>
2023-08-03 18:02:25 +0200

    move docs to subdirectory

--- docs/conf.py
+++ docs/conf.py
@@ -3,6 +3,8 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import sys
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -13,7 +15,9 @@ author = 'Jonas Otto'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ['sphinx.ext.autodoc']
+autoclass_content = 'both'
+sys.path.insert(0, 'orchestrator/ros2/orchestrator')
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
