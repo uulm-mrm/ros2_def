74469a3a2ee8a8cacf32d42df6dce7d9f9649c4c
Jonas Otto <jonas@jonasotto.com>
2023-08-25 15:46:07 +0000

    more type annotations

--- ros2/orchestrator/orchestrator/orchestrator_lib/model_loader.py
+++ ros2/orchestrator/orchestrator/orchestrator_lib/model_loader.py
@@ -1,3 +1,5 @@
+# pyright: strict
+
 from pathlib import Path
 from typing import List, Dict, Any
 
@@ -64,7 +66,7 @@ def load_node_config(package: str, name: str, schema: JsonSchema):
     return load_node_config_file(path, schema)
 
 
-def load_launch_config_file(launch_config_path, schema: JsonSchema):
+def load_launch_config_file(launch_config_path: Path, schema: JsonSchema):
     with open(launch_config_path) as f:
         launch_config = json.load(f)
     validate(instance=launch_config, schema=schema)
@@ -79,7 +81,7 @@ def load_launch_config(package: str, name: str, schema: JsonSchema):
     return load_launch_config_file(path, schema)
 
 
-def load_models(launch_config: dict, node_config_schema: JsonSchema) -> List[NodeModel]:
+def load_models(launch_config: dict[str, Any], node_config_schema: JsonSchema) -> List[NodeModel]:
     models: list[NodeModel] = []
     for name, node in launch_config["nodes"].items():
         remappings: Dict[str, str] = node.get("remappings", {})
