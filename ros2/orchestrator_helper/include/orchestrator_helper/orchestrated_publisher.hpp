083ca767409969be6415f725d7e2b296a4ed468c
Jan Strohbeck <jan.strohbeck@uni-ulm.de>
2023-09-11 07:42:12 +0000

    Add some documentation for C++ helper classes.

--- ros2/orchestrator_helper/include/orchestrator_helper/orchestrated_publisher.hpp
+++ ros2/orchestrator_helper/include/orchestrator_helper/orchestrated_publisher.hpp
@@ -2,19 +2,38 @@
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_lifecycle/lifecycle_node.hpp>
 
+/**
+ * Wrapper class for a LifecyclePublisher. Counts the number of \ref publish calls made since the last counter reset.
+ * @tparam MessageT The type of the message that the wrapped publisher can publish.
+ * @tparam AllocT Allocator
+ */
 template <typename MessageT, typename AllocT = std::allocator<void>>
 class OrchestratedPublisher
 {
 public:
+  /**
+   * Type alias for a SharedPtr to a MessageT LifecyclePublisher publisher.
+   */
   using PublisherPtr = rclcpp_lifecycle::LifecyclePublisher<MessageT, AllocT>::SharedPtr;
+  /**
+   * Type alias for an std::shared_ptr to an instance of OrchestratedPublisher.
+   */
   using SharedPtr = std::shared_ptr<OrchestratedPublisher<MessageT, AllocT>>;
 
+  /**
+   * Constructor. Initializes the internal publish counter to zero.
+   * @param publisher Pointer to a LifecyclePublisher instance that performs the actual publishing.
+   */
   explicit OrchestratedPublisher(PublisherPtr publisher)
   {
     publisher_ = publisher;
     reset_publish_cnt();
   }
 
+  /**
+   * Publish a message via the wrapped LifecyclePublisher instance, and increments the internal counter.
+   * @param msg The message to publish
+   */
   template <typename M>
   void publish(M msg)
   {
@@ -22,26 +41,41 @@ public:
     publish_cnt_++;
   }
 
+  /**
+   * Resets the internal publish counter to zero.
+   */
   void reset_publish_cnt()
   {
     publish_cnt_ = 0;
   }
 
-  std::size_t get_publish_cnt()
+  /**
+   * Obtain the value of the internal publish counter.
+   */
+  [[nodiscard]] std::size_t get_publish_cnt()
   {
     return publish_cnt_;
   }
 
-  auto get_topic_name()
+  /**
+   * Returns the name of the topic that the wrapped publisher is publishing on.
+   */
+  [[nodiscard]] auto get_topic_name()
   {
     return publisher_->get_topic_name();
   }
 
+  /**
+   * Calls on_activate on the wrapped LifecyclePublisher.
+   */
   void on_activate()
   {
     publisher_->on_activate();
   }
 
+  /**
+   * Calls on_deactivate on the wrapped LifecyclePublisher.
+   */
   void on_deactivate()
   {
     publisher_->on_deactivate();
--- ros2/orchestrator_helper/include/orchestrator_helper/orchestrator_helper.hpp
+++ ros2/orchestrator_helper/include/orchestrator_helper/orchestrator_helper.hpp
@@ -4,6 +4,9 @@
 #include <orchestrator_interfaces/msg/status.hpp>
 #include <orchestrator_helper/orchestrated_publisher.hpp>
 
+/***
+ * Helper class for easier publishing of status messages on ROS callbacks.
+ */
 class OrchestratorHelper
 {
 private:
@@ -13,26 +16,55 @@ private:
   using LifecyclePublisherPtr = rclcpp_lifecycle::LifecyclePublisher<MessageT, AllocT>::SharedPtr;
 
 public:
+  /**
+   * Type alias for an std::shared_ptr to an instance of OrchestratorHelper.
+   */
   using SharedPtr = std::shared_ptr<OrchestratorHelper>;
 
+  /**
+   * Constructor.
+   * @param node Pointer to a LifecycleNode which will be used to create publishers.
+   */
   explicit OrchestratorHelper(LifecycleNodePtr node) : node_(node)
   {
   }
 
-  template <typename MessageT, typename... Params>
+  /**
+   * Creates a LifecyclePublisher wrapped in an OrchestratedPublisher instance.
+   * @param params Any params that shall be forwarded to the create_publisher method of the node.
+   * @return The wrapped OrchestratedPublisher instance.
+   * @tparam MessageT The type of the message that the wrapped publisher can publish.
+   * @tparam AllocT Allocator
+   */
+  template <typename MessageT, typename... Params, typename AllocT = std::allocator<void>>
   auto create_publisher(Params&&... params)
   {
-    auto publisher = node_->create_publisher<MessageT>(std::forward<Params>(params)...);
-    // decltype(*publisher)::ROSMessageTypeAllocatorTraits::allocator_type
-    return wrap_publisher<MessageT, std::allocator<void>>(std::move(publisher));
+    auto publisher = node_->create_publisher<MessageT, AllocT>(std::forward<Params>(params)...);
+    return wrap_publisher<MessageT, AllocT>(std::move(publisher));
   }
 
+  /**
+   * Wraps an existing LifecyclePublisher in an OrchestratedPublisher instance.
+   * @param publisher Pointer to the LifecyclePublisher instance that shall be wrapped.
+   * @tparam MessageT The type of the message that the wrapped publisher can publish.
+   * @tparam AllocT Allocator
+   */
   template <typename MessageT, typename AllocT>
   auto wrap_publisher(LifecyclePublisherPtr<MessageT, AllocT> publisher)
   {
     return std::make_shared<OrchestratedPublisher<MessageT, AllocT>>(std::move(publisher));
   }
 
+  /**
+   * Wraps a ROS callback. Will publish status calls signaling to the orchestrator that a callback has completed if it
+   * did not produce any outputs or if any of the passed publishers did not publish during the callback.
+   * This is a variant in which the callback takes no parameters (e.g., a timer callback).
+   * @param publishers OrchestratedPublisher instances that can publish messages during the callback
+   * @param f ROS callback function which may call publish() on the passed publishers.
+   * @return The wrapped callback function
+   * @tparam CallbackT Type of the callback
+   * @tparam P Publisher types
+   */
   template <typename CallbackT, typename... P>
   std::function<void(void)> wrap_callback(CallbackT&& f, P&&... publishers)
   {
@@ -49,6 +81,16 @@ public:
     };
   }
 
+  /**
+   * Wraps a ROS callback. Will publish status calls signaling to the orchestrator that a callback has completed if it
+   * did not produce any outputs or if any of the passed publishers did not publish during the callback.
+   * This is a variant in which the callback takes a message as a parameter (e.g., a subscription callback).
+   * @param publishers OrchestratedPublisher instances that can publish messages during the callback
+   * @param f ROS callback function which may call publish() on the passed publishers.
+   * @return The wrapped callback function
+   * @tparam CallbackT Type of the callback
+   * @tparam P Publisher types
+   */
   template <typename MessageT, typename CallbackT, typename... P>
   std::function<void(typename MessageT::ConstSharedPtr)> wrap_callback(CallbackT&& f, P&&... publishers)
   {
@@ -65,6 +107,11 @@ public:
     };
   }
 
+  /**
+   * Publishes a status message for the orchestrator.
+   * @param omitted_outputs List of omitted outputs (topics of publishers that did not publish although it is expected by the orchestrator).
+   * @param debug_id An ID for the status message to help debugging the orchestrator. Values less than 0 produce auto-incrementing debug IDs.
+   */
   void publish_status(const std::vector<std::string>& omitted_outputs = {}, int debug_id = -1)
   {
     static int debug_id_ = 0;
@@ -75,21 +122,33 @@ public:
     status_pub_->publish(std::move(status_msg));
   }
 
+  /**
+   * Advertises topics.
+   */
   void advertise()
   {
     status_pub_ = node_->create_publisher<StatusMsg>("/status", 10);
   }
 
+  /**
+   * Activates internal LifecyclePublishers.
+   */
   void on_activate()
   {
     status_pub_->on_activate();
   }
 
+  /**
+   * Deactivates internal LifecyclePublishers.
+   */
   void on_deactivate()
   {
     status_pub_->on_deactivate();
   }
 
+  /**
+   * Destroys internal LifecyclePublishers.
+   */
   void on_cleanup()
   {
     status_pub_.reset();
